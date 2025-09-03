abeb7520d2d37ca1ad7e295df35c6e30d8b92c99
Artemidus <zhuravlev89artem@gmail.com>
2025-09-03 22:29:07 +0400

    Initial commit

---
+++ src/test/java/ru/projectteamwork/finance_recommendations/FinanceRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.projectteamwork.finance_recommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceRecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
