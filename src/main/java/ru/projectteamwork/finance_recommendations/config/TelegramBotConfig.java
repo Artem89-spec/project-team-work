6adb004602527733fa5cd12e8a4e062a947874e2
Artemidus <zhuravlev89artem@gmail.com>
2025-10-07 00:45:46 +0400

    проверяю реквест

--- src/main/java/ru/projectteamwork/finance_recommendations/config/TelegramBotConfig.java
+++ src/main/java/ru/projectteamwork/finance_recommendations/config/TelegramBotConfig.java
@@ -4,14 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.util.WebhookUtils;
+import ru.projectteamwork.finance_recommendations.telegram.RecommendationTelegramBot;
 
 @Configuration
 public class TelegramBotConfig {
+
+    private final RecommendationTelegramBot recommendationTelegramBot;
+
+    public TelegramBotConfig(RecommendationTelegramBot recommendationTelegramBot) {
+        this.recommendationTelegramBot = recommendationTelegramBot;
+    }
+
     @Bean
-    public TelegramBotsApi telegramBotsApi(TelegramLongPollingBot bot) throws Exception {
+    public TelegramBotsApi telegramBotsApi() throws Exception {
+        // Перед регистрацией бота пытаемся очистить webhook
+        try {
+            recommendationTelegramBot.clearWebhook();
+        } catch (Exception e) {
+            // Игнорируем ошибки, связанные с отсутствием webhook
+            System.out.println("Webhook уже не установлен или не удалось его очистить: " + e.getMessage());
+        }
+
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(bot);
+        botsApi.registerBot(recommendationTelegramBot);
         return botsApi;
     }
 }
